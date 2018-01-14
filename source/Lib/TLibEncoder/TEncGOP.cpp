e4b8c18e900ee46cefd7f555abc554a3b72dd31d
He,Xiaoyi <xiaoyi.he@outlook.com>
2018-01-14 14:17:38 +0800

    bug fix

--- source/Lib/TLibEncoder/TEncGOP.cpp
+++ source/Lib/TLibEncoder/TEncGOP.cpp
@@ -52,6 +52,85 @@
 #include <deque>
 using namespace std;
 
+double getMSSIM( const cv::Mat& i1, const cv::Mat& i2)
+{
+  
+  const double C1 = 6.5025, C2 = 58.5225;
+  /***************************** INITS **********************************/
+  
+  
+  cv::Mat I1, I2;
+  i1.convertTo(I1,CV_8UC1,255);
+  i2.convertTo(I2,CV_8UC1,255);
+  I1.convertTo(I1,CV_32F);
+  I2.convertTo(I2,CV_32F);
+  
+  cv::Mat I2_2   = I2.mul(I2);        // I2^2
+  cv::Mat I1_2   = I1.mul(I1);        // I1^2
+  cv::Mat I1_I2  = I1.mul(I2);        // I1 * I2
+  
+  /*************************** END INITS **********************************/
+  
+  cv::Mat mu1, mu2;   // PRELIMINARY COMPUTING
+  //int kernel_size = 8;
+  //cv::Mat kernel = cv::Mat::ones( kernel_size, kernel_size, CV_32F )/ (float)(kernel_size*kernel_size);
+  
+  /// Apply filter
+  //filter2D(I1, mu1, -1 , kernel);
+  //filter2D(I2, mu2, -1 , kernel);
+  cv::GaussianBlur(I1, mu1, cv::Size(11, 11), 1.5);
+  cv::GaussianBlur(I2, mu2, cv::Size(11, 11), 1.5);
+  
+  cv::Mat mu1_2   =   mu1.mul(mu1);
+  cv::Mat mu2_2   =   mu2.mul(mu2);
+  cv::Mat mu1_mu2 =   mu1.mul(mu2);
+  
+  cv::Mat sigma1_2, sigma2_2, sigma12;
+  //filter2D(I1_2, sigma1_2, -1 , kernel);
+  cv::GaussianBlur(I1_2, sigma1_2, cv::Size(11, 11), 1.5);
+  sigma1_2 -= mu1_2;
+  //filter2D(I2_2, sigma2_2, -1 , kernel);
+  cv::GaussianBlur(I2_2, sigma2_2, cv::Size(11, 11), 1.5);
+  sigma2_2 -= mu2_2;
+  //filter2D(I1_2, sigma12, -1 , kernel);
+  cv::GaussianBlur(I1_I2, sigma12, cv::Size(11, 11), 1.5);
+  sigma12 -= mu1_mu2;
+  
+  ///////////////////////////////// FORMULA ////////////////////////////////
+  cv::Mat t1, t2, t3;
+  
+  t1 = 2 * mu1_mu2 + C1;
+  t2 = 2 * sigma12 + C2;
+  t3 = t1.mul(t2);              // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+  
+  t1 = mu1_2 + mu2_2 + C1;
+  t2 = sigma1_2 + sigma2_2 + C2;
+  t1 = t1.mul(t2);               // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+  
+  cv::Mat ssim_map;
+  divide(t3, t1, ssim_map);      // ssim_map =  t3./t1;
+  
+  cv::Scalar mssim = mean( ssim_map ); // mssim = average of ssim map
+  return mssim[0];
+}
+
+double getPSNR(const cv::Mat& I1,const cv::Mat& I2)
+{
+  cv::Mat s1;
+  cv::absdiff(I1,I2,s1);
+  s1 = s1.mul(s1);
+  cv::Scalar s = sum(s1);
+  double sse = s.val[0];
+  //cout<< int(sse *255 *255) << endl;
+  if(sse<=1e-10)
+    return 0;
+  else {
+    double mse = sse / (double)(I1.total());
+    double psnr = 10.0*log10((1)/mse);
+    return psnr;
+  }
+}
+
 #if JVET_D0033_ADAPTIVE_CLIPPING
 namespace {
 // simulate a coding decoding of the Intra(intrinsic) prms: to be sync with real encoding
@@ -2218,8 +2297,8 @@ Void TEncGOP::compressGOP( Int iPOCLast, Int iNumPicRcvd, TComList<TComPic*>& rc
   TComPicYuv cscd;
   if (conversion != IPCOLOURSPACE_UNCHANGED)
   {
-      cscd.create(pcPicD->getWidth(COMPONENT_Y), pcPicD->getHeight(COMPONENT_Y), pcPicD->getChromaFormat(), g_uiMaxCUWidth, g_uiMaxCUHeight, g_uiMaxCUDepth);
-      TVideoIOYuv::ColourSpaceConvert(*pcPicD, cscd, conversion, g_bitDepth, false);
+      cscd.create(pcPicD->getWidth(COMPONENT_Y), pcPicD->getHeight(COMPONENT_Y), pcPicD->getChromaFormat(),pcPicD->getWidth(COMPONENT_Y), pcPicD->getHeight(COMPONENT_Y), 0, false);
+      TVideoIOYuv::ColourSpaceConvert(*pcPicD, cscd, conversion, false);
   }
   TComPicYuv &picd = (conversion == IPCOLOURSPACE_UNCHANGED) ? *pcPicD : cscd;
 
